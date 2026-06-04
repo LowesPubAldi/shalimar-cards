8c1cbf86a8a91598a34f40128249fba9f86c00a2
LowesPubAldi <jovanminsel@gmail.com>
2026-06-04 15:17:19 -0500

    Complete routing setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function Navbar () {
-  return <h1>Shalimar Cards</h1>;    
+  return (
+    <nav classname="navbar">
+      <h1>Shalimar Cards</h1>
+
+      <div classname="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/catalog">Catalog</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </nav>
+  );    
 }
 
 export default Navbar;
