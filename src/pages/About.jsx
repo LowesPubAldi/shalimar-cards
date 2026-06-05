33d74efb9931d9035964741b71c1a110af72bea6
LowesPubAldi <jovanminsel@gmail.com>
2026-06-05 18:24:16 -0500

    Styling Fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,19 @@
 function About () {
-    return <h1>About</h1>;
-}
+    return ( 
+    <div>
+    <h1>About Shalimar Cards</h1>
+<p>
+  Shalimar Cards is a React project built using the YGOProDeck API.
+</p>
 
+<p>
+  Users can search, sort, and view detailed information about hundreds of cards.
+</p>
+
+<p>
+  This project is a foundation for a future trading card website.
+</p>
+</div>
+);
+}
 export default About;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,5 +1,12 @@
 function Catalog () {
-    return <h1>Catalog</h1>;
+    return (
+    <div>
+    <h1>Catalog</h1>
+<p>
+  Browse hundreds of Yu-Gi-Oh! cards pulled from the YGOProDeck API.
+</p>
+</div> 
+);
 }
 
 export default Catalog;
