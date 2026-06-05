b51963ffd15ac264001c8ea6243bfd9e6594d034
LowesPubAldi <jovanminsel@gmail.com>
2026-06-05 13:05:05 -0500

    Added card sorting functionality

--- src/components/Cardgrid.jsx
+++ src/components/Cardgrid.jsx
@@ -5,6 +5,8 @@ function Cardgrid ({ search = '' }) {
     const [cards, setCards] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [sortOrder, setSortOrder] = useState('');
+
     
     useEffect(() => {
         const fetchCards = async () => {
@@ -40,12 +42,44 @@ function Cardgrid ({ search = '' }) {
         return <p>{error}</p>;
     }
 
+    const sortedCards = [...filteredCards].sort((a, b) => {
+        if (sortOrder === 'az'){
+            return a.name.localeCompare(b.name);
+        }    
+    
+        if (sortOrder === 'za'){
+            return b.name.localeCompare(a.name);
+        }
+
+        if (sortOrder === 'atkHigh') {
+            return (b.atk ?? -1) - (a.atk ?? -1);
+        }
+
+        if (sortOrder === 'atkLow') {
+            return (a.atk ?? 99999 ) - (b.atk ?? 99999);
+        }
+
+        return 0;
+    });
+
     return (
+        <>
+        <select 
+        value={sortOrder}
+        onChange={(event) => setSortOrder(event.target.value)}>
+            <option value="">Sort by</option>
+            <option value="az">A-Z</option>
+            <option value="za">Z-A</option>
+            <option value="atkHigh">Atk High-Low</option>
+            <option value="atkLow">Atk Low-High</option>
+        </select>
+
         <div className="card-grid">
-            {filteredCards.map((card) => (
+            {sortedCards.map((card) => (
                 <Card key={card.id} card={card} />
             ))}
         </div>
+        </>
     );}
 
     export default Cardgrid;
